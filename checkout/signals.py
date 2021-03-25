36cf1e679c235f068fd9d1e1dbcae58880a4b3de
upeshp <upesh.patel@hotmail.com>
2021-03-25 17:31:18 +0000

    Feat: added checkout success template and logic

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,3 +1,4 @@
+
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
@@ -11,7 +12,7 @@ def update_on_save(sender, instance, created, **kwargs):
     instance.order.update_total()
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
