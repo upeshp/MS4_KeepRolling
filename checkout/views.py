bf542b4414660536a4d071a06b2c0ab7b6fe5a12
upeshp <upesh.patel@hotmail.com>
2021-03-25 10:37:47 +0000

    Feat: added stripe elements on checkout.html for card payments, and js/css in checkout app static folder to enable this functionality

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IYpfLJSg9iTtq48NgH5Tx0HAjJAxTISngSrMYpHuB55xLrIMcZS1OjCuUUT0ZNiJHzneaijyYOL3I8hUPBy0nZb00PTDO7aKA',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
