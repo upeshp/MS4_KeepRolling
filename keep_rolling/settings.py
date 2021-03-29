689240f0d7c3d3128b34bf1245397400a81347f1
upeshp <upesh.patel@hotmail.com>
2021-03-29 20:07:08 +0000

    Deployment: removed secret key and set debug

--- keep_rolling/settings.py
+++ keep_rolling/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h(q)cg$)s473x3t2sg9t5ffzfq6eu-azry*0=r!uum1)4r8-rs'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms4-keep-rolling.herokuapp.com', 'localhost']
 
