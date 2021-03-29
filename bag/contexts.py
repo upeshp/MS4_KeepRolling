22de8d16682cdba5d768cc3566ba824debb8d2c0
upeshp <upesh.patel@hotmail.com>
2021-03-29 10:09:07 +0000

    Feat: changed delivery variable to 0 on bag contexts.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -36,7 +36,7 @@ def bag_contents(request):
     #     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
     #     free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     # else:
-    #     delivery = 0
+    delivery = 0
     #     free_delivery_delta = 0
 
     grand_total = total
@@ -45,7 +45,7 @@ def bag_contents(request):
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
-        # 'delivery': delivery,
+        'delivery': delivery,
         # 'free_delivery_delta': free_delivery_delta,
         # 'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
